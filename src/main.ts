e3d1096e13854b758564cf8e47350548476c3489
qb1ty <arsenbro0707@mail.ru>
2026-09-08 14:18:05 +0500

    feat(app): implement application entry point with gRPC microservice bootstrapping

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { ConfigService } from "@nestjs/config"
+import { NestFactory } from "@nestjs/core"
+
+import { AppModule } from "./app.module"
+import type { AllConfigs } from "./config/interfaces"
+import { createGrpcServer } from "./infrastructure/grpc"
+
+async function bootstrap() {
+	const app = await NestFactory.create(AppModule)
+
+	const config = app.get(ConfigService<AllConfigs>)
+
+	createGrpcServer(app, config)
+
+	await app.startAllMicroservices()
+	await app.init()
+}
+bootstrap()
