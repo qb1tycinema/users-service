4a280ec55213b8adbdb5cb3f8b85434da89c41c0
qb1ty <arsenbro0707@mail.ru>
2026-09-10 22:04:53 +0500

    feat(config): define EnvironemtConfig TypeScript interface

--- src/config/interfaces/all-config.interface.ts
+++ src/config/interfaces/all-config.interface.ts
@@ -1,5 +1,9 @@
+import type { DatabaseConfig } from "./database.interface"
+import type { EnvironemtConfig } from "./environemt.interface"
 import type { GrpcConfig } from "./grpc.interface"
 
 export interface AllConfigs {
+	database: DatabaseConfig
+	environemt: EnvironemtConfig
 	grpc: GrpcConfig
 }
---
+++ src/config/interfaces/environemt.interface.ts
@@ -0,0 +1,5 @@
+import { Environemt } from "../validators"
+
+export interface EnvironemtConfig {
+	environemt: Environemt
+}
