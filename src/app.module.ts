9b069a849857ad1436caf891c0f6651d6b63f262
qb1ty <arsenbro0707@mail.ru>
2026-09-10 22:06:41 +0500

    feat(app): register database, environment, and gRPC config factories in global ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,13 @@
 import { Module } from "@nestjs/common"
 import { ConfigModule } from "@nestjs/config"
 
-import { grpcEnv } from "./config/env"
+import { databaseEnv, environemtEnv, grpcEnv } from "./config/env"
 
 @Module({
 	imports: [
 		ConfigModule.forRoot({
 			isGlobal: true,
-			load: [grpcEnv]
+			load: [databaseEnv, environemtEnv, grpcEnv]
 		})
 	]
 })
