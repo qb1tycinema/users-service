5eb7a1d06bc150999ed506d7664cdb7f72391cb1
qb1ty <arsenbro0707@mail.ru>
2026-09-10 22:57:17 +0500

    feat(app): integrate DatabaseModule and UsersModule into root AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,17 @@ import { Module } from "@nestjs/common"
 import { ConfigModule } from "@nestjs/config"
 
 import { databaseEnv, environemtEnv, grpcEnv } from "./config/env"
+import { DatabaseModule } from "./infrastructure/database/database.module"
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
 	imports: [
 		ConfigModule.forRoot({
 			isGlobal: true,
 			load: [databaseEnv, environemtEnv, grpcEnv]
-		})
+		}),
+		DatabaseModule,
+		UsersModule
 	]
 })
 export class AppModule {}
