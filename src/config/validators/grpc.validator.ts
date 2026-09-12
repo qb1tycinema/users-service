71e9259297a19ce68101485169157ae193672494
qb1ty <arsenbro0707@mail.ru>
2026-09-12 22:09:41 +0500

    fix(config): update GrpcValidator to use ACCOUNT_GRPC_URL with correct regex validation

--- src/config/validators/grpc.validator.ts
+++ src/config/validators/grpc.validator.ts
@@ -1,9 +1,18 @@
-import { IsInt, IsString } from "class-validator"
+import { IsInt, IsNotEmpty, IsString, Matches } from "class-validator"
 
 export class GrpcValidator {
 	@IsString()
+	@IsNotEmpty()
 	public GRPC_HOST!: string
 
 	@IsInt()
 	public GRPC_PORT!: number
+
+	@IsString()
+	@IsNotEmpty()
+	@Matches(/^.+:\d{1,5}$/, {
+		message:
+			"ACCOUNT_GRPC_URL must be in the format 'host:port' (e.g., localhost:50051)"
+	})
+	public ACCOUNT_GRPC_URL!: string
 }
