281fabf216768fb530e2090ba86654eebd1bcfb9
qb1ty <arsenbro0707@mail.ru>
2026-09-12 22:12:16 +0500

    feat(users): implement getMe method aggregating profile from db and account data via gRPC

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,14 +1,50 @@
 import { Injectable } from "@nestjs/common"
-import type { CreateUserRequest } from "@qb1tycinema/contracts/gen/users"
+import type { CreateUserRequest, GetMeRequest } from "@qb1tycinema/contracts/gen/users"
 
 import { UsersRepository } from "./users.repository"
+import { AccountClientGrpc } from "@/infrastructure/grpc/clients/account.client"
+import { RpcException } from "@nestjs/microservices"
+import { RpcStatus } from "@qb1tycinema/common"
+import { lastValueFrom } from "rxjs"
 
 @Injectable()
 export class UsersService {
-	public constructor(private readonly usersRepository: UsersRepository) {}
+	public constructor(
+		private readonly usersRepository: UsersRepository,
+		private readonly accountClient: AccountClientGrpc
+	) {}
+
+	public async getMe(data: GetMeRequest) {
+		const { id } = data
+
+		const profile = await this.usersRepository.findById(id)
+
+		if (!profile) {
+			throw new RpcException({
+				code: RpcStatus.NOT_FOUND,
+				details: "Profile not found"
+			})
+		}
+
+		const account = await lastValueFrom(
+			this.accountClient.getAccount({ id: id })
+		)
+
+		return {
+			user: {
+				id: profile.id,
+				name: profile.name ?? undefined,
+				avatar: profile.avatar ?? undefined,
+				email: account.email,
+				phone: account.phone
+			}
+		}
+	}
 
 	public async create(data: CreateUserRequest) {
-		await this.usersRepository.create({ id: data.id })
+		const { id } = data
+
+		await this.usersRepository.create({ id: id })
 
 		return { ok: true }
 	}
