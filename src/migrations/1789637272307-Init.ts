993fa6ccbd276f6d24e2fd44e7992da886843864
qb1ty <arsenbro0707@mail.ru>
2026-09-17 14:46:06 +0500

    chore(db): add initial users table migration

---
+++ src/migrations/1789637272307-Init.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Init1789637272307 implements MigrationInterface {
+    name = 'Init1789637272307'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "users" ("id" character varying NOT NULL, "name" character varying, "avatar" character varying, "created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "users"`);
+    }
+
+}
